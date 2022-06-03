24ad9083c8ab967d02ee8547804c84cbcfe41183
mrjerry908 <106414484+mrjerry908@users.noreply.github.com>
2022-06-03 09:06:46 +0800

    Create operates.go

---
+++ blockchain_golang/database/operates.go
@@ -0,0 +1,114 @@
+package database
+
+import (
+	"errors"
+	"github.com/btltdb/bolt"
+	log "github.com/corgi-kx/tlogcustom"
+)
+
+
+func (bd *BlockchainDB) Put(k, v []byte, bt BucketType) {
+	var DBFileName = "blockchain_" + ListenPort + ".db"
+	db, err := bolt.Open(DBFileName, 0600, nil)
+	defer db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bt))
+		if bucket == nil {
+			var err error
+			bucket, err = tx.CreateBucket([]byte(bt))
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+		err := bucket.Put(k, v)
+		if err != nil {
+			log.Panic(err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+
+func (bd *BlockchainDB) View(k []byte, bt BucketType) []byte {
+	var DBFileName = "blockchain_" + ListenPort + ".db"
+	db, err := bolt.Open(DBFileName, 0600, nil)
+	defer db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	result := []byte{}
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bt))
+		if bucket == nil {
+			msg := "datebase view warnning" + string(bt)
+			return errors.New(msg)
+		}
+		result = bucket.Get(k)
+		return nil
+	})
+	if err != nil {
+		//log.Warn(err)
+		return nil
+	}
+
+	realResult := make([]byte, len(result))
+	copy(realResult, result)
+	return realResult
+}
+
+
+func (bd *BlockchainDB) Delete(k []byte, bt BucketType) bool {
+	var DBFileName = "blockchain_" + ListenPort + ".db"
+	db, err := bolt.Open(DBFileName, 0600, nil)
+	defer db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bt))
+		if bucket == nil {
+			msg := "datebase delete warnning: + string(bt)
+			return errors.New(msg)
+		}
+		err := bucket.Delete(k)
+		if err != nil {
+			log.Panic(err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	return true
+}
+
+
+func (bd *BlockchainDB) DeleteBucket(bt BucketType) bool {
+	var DBFileName = "blockchain_" + ListenPort + ".db"
+	db, err := bolt.Open(DBFileName, 0600, nil)
+	defer db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		err := tx.DeleteBucket([]byte(bt))
+		if err != nil {
+			log.Panic(err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return true
+}
