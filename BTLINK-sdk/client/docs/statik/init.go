467b8cd2cb3306e0a806b01c3ea74fae1fd54029
mrjerry908 <106414484+mrjerry908@users.noreply.github.com>
2022-06-12 21:52:06 +0800

    Create init.go

---
+++ BTLINK-sdk/client/docs/statik/init.go
@@ -0,0 +1,122 @@
+{
+	"swagger": "2.0",
+	"info": {
+	"title": "btlink SDK - gRPC Gateway docs",
+	"description": "A REST interface for state queries",
+	"version": "1.0.0"
+	},
+	"apis": [
+	{
+	"url": "./tmp-swagger-gen/btlink/auth/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "AuthParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/bank/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "BankParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/base/tendermint/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "BaseParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/distribution/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "DistributionParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/evidence/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "EvidenceParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/gov/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "GovParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/mint/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "MintParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/params/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "Params"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/slashing/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "SlashingParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/staking/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "StakingParams",
+	"DelegatorValidators": "StakingDelegatorValidators"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/tx/v1beta1/service.swagger.json",
+	"dereference": {
+	"circular": "ignore"
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/upgrade/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "UpgradeParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/authz/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "AuthzParams"
+	}
+	}
+	},
+	{
+	"url": "./tmp-swagger-gen/btlink/feegrant/v1beta1/query.swagger.json",
+	"operationIds": {
+	"rename": {
+	"Params": "FeegrantParams"
+	}
+	}
+	}
+	]
+	}
+
