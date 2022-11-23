7fd82bfb3dabb6ec88b0af7872f9100ac3b826fa
mrjerry908 <106414484+mrjerry908@users.noreply.github.com>
2022-11-23 08:58:40 +0800

    Create data.js

---
+++ blockchain_golang/data.js
@@ -0,0 +1,186 @@
+Let Data = {
+  ZX: [[
+    {{
+      name: 'EOS/USDT', // Transaction pair
+      Exchange: 'Tiger', // Exchange
+      Vol: '910892.3 million', // Transaction volume
+      Price: '77 .49 CNY ',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+2.78%', //
+        Price: '78 .20 CNY '// Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+1.92%', //
+        Price: '71 .90 CNY '// Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '-0.27%', //
+        Price: '76 .21 CNY '// Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+0.66%', //
+        Price: '86 .77 CNY '// Price
+      }]
+    },
+    {{
+      name: 'LTC/usdt', // Transaction pair
+      Exchange: 'Fire Coin', // Exchange
+      Vol: '41172743 million', // Transaction volume
+      Price: '727.33',,
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '-1.59%', //
+        Price: '832.83 CNY' // Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+2.48%', //
+        Price: '860.99 CNY' // Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '+0.27%', //
+        Price: '758.69 CNY' // Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+12.88%', //
+        Price: '891.09 CNY' // Price
+      }]
+    },
+    {{
+      name: 'Eth/BTC', // Transaction pair
+      Exchange: 'Tiger', // Exchange
+      Vol: '13727.43 million', // Transaction volume
+      Price: '3477.90 CNY',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+6.66%', //
+        Price: '3488.28 CNY' // Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+4.76%', //
+        Price: '3450.79 CNY' // Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '+9.11%', //
+        Price: '3500.78 CNY' // Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+3.19%', //
+        Price: '3499.35 CNY' // Price
+      }]
+    },
+    {{
+      name: 'OMG/BTC', // Transaction pair
+      Exchange: 'OKEX', // Exchange
+      Vol: '98923 million', // Transaction volume
+      Price: '64.93 CNY ',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+1.35%', //
+        Price: '64 .28 CNY '// Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+0.86%', //
+        Price: '64 .28 CNY '// Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '-6.55%', //
+        Price: '58 .29 CNY '// Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+6.88%', //
+        Price: '75 .29 CNY '// Price
+      }]
+    },
+    {{
+      name: 'XRP/BTC', // Transaction pair
+      Exchange: 'Tiger', // Exchange
+      Vol: '9602.3 million', // Transaction volume
+      Price: '64.93 CNY ',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+1.35%', //
+        Price: '64 .28 CNY '// Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+0.86%', //
+        Price: '64 .28 CNY '// Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '-6.55%', //
+        Price: '58 .29 CNY '// Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+6.88%', //
+        Price: '75 .29 CNY '// Price
+      }]
+    }
+  ],,,
+  BTC: [[
+    {{
+      name: 'BCH/BTC', // Transaction pair
+      Exchange: 'Fire Coin', // Exchange
+      Vol: '14722.13 million', // Transaction volume
+      Price: '6084.43 CNY',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+2.98%', //
+        Price: '6084.43 CNY' // Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+3.02%', //
+        Price: '6090.80 CNY' // Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '+2.08%', //
+        Price: '6060.34 CNY' // Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+2.34%', //
+        Price: '6072.54 CNY' // Price
+      }]
+    },
+    {{
+      name: 'Eth/BTC', // Transaction pair
+      Exchange: 'Tiger', // Exchange
+      Vol: '13727.43 million', // Transaction volume
+      Price: '3477.90 CNY',
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '+6.66%', //
+        Price: '3488.28 CNY' // Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+4.76%', //
+        Price: '3450.79 CNY' // Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '+9.11%', //
+        Price: '3500.78 CNY' // Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+3.19%', //
+        Price: '3499.35 CNY' // Price
+      }]
+    },
+    {{
+      name: 'LTC/BTC', // Transaction pair
+      Exchange: 'Fire Coin', // Exchange
+      Vol: '11727.43 million', // Transaction volume
+      Price: '727.33',,
+      exchangelist: [{{
+        exchangename: 'Firecoin', // The name of the exchange
+        upandowm: '-2.87%', //
+        Price: '732.83 CNY' // Price
+      }, {{
+        exchangename: 'Tiger', // The name of the exchange
+        upandowm: '+1.76%', //
+        Price: '760.75 CNY' // Price
+      }, {{
+        exchangename: 'Bit-Z', // The name of the exchange
+        upandowm: '+0.27%', //
+        Price: '748.31 CNY' // Price
+      }, {{
+        exchangename: 'OKEX', // The name of the exchange
+        upandowm: '+12.88%', //
+        Price: '791.
