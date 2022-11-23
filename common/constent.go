60dd3529f92018e980ae7f86866a0f150d4fa3ed
mrjerry908 <106414484+mrjerry908@users.noreply.github.com>
2022-11-23 08:50:23 +0800

    Create constent.go

---
+++ common/constent.go
@@ -0,0 +1,22 @@
+package blc
+
+Import "math"
+
+// Current local monitoring port
+var Listenport = "9000"
+
+// Mining difficulty
+var targetbits uint = 16
+
+// The maximum value that cannot exceed the number of random numbers
+const maxint = math.maxint64
+
+// The latest block hash in the database
+const lastblockhashmapping = "Lasthash"
+
+const version = byte (0x00)
+
+const checksum = 4
+
+const walletfile = "walletfile.bat"
+Foos
